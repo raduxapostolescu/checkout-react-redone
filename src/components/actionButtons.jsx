b8357e7414a8d1e9b1a81f5735d22be1a4529bdd
Radu Apostolescu <raduxapostolescu@gmail.com>
2022-04-07 14:18:24 +0200

    set action buttons margin top

--- src/components/actionButtons.jsx
+++ src/components/actionButtons.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import ActionButton from "./actionButton";
+import "../styles/actionButtons.css";
 
 class ActionButtons extends Component {
   render() {
     return (
-      <div className="d-flex flex-column justify-content-end align-items-end w-50">
+      <div className="actionButtons d-flex flex-column justify-content-end align-items-end w-50">
         {this.props.buttons.map((button) => (
           <ActionButton
             button={button}
